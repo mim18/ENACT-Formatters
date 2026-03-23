f46205750893a4b52308348636d6c48b565a4247
Kevin Bui <kvb2univpitt@gmail.com>
2026-03-23 02:46:08 -0400

    Rename to breakdown file.

--- Table-One/create_sample_data.sh
+++ Table-One/create_sample_data.sh
@@ -25,11 +25,11 @@
 mkdir data
 
 cp Formatter_age45to54.csv data/age_45to54.csv
-cp formatteralldem.csv data/all_dem.csv
+cp formatteralldem.csv data/breakdown_dem.csv
 cp Formatter\ amantadine.csv data/amantadine.csv
 cp Formatter\ baclofen.csv data/baclofen.csv
 cp formattercohortnovmat2medalpha.csv data/no_vmat2_medalpha.csv
-cp Formatter\ _elix.csv data/elix.csv
+cp Formatter\ _elix.csv data/breakdown_elix.csv
 cp Formatter\ female.csv data/female.csv
 cp Formatter\ haloperidol.csv data/haloperidol.csv
 cp Formatter\ no_vmat2_age45to54.csv data/no_vmat2_age_45to54.csv
@@ -40,7 +40,7 @@ cp Formatter_No_VMAT2_halperidol.csv data/no_vmat2_halperidol.csv
 cp Formatter\ vmat2_age45to54.csv data/vmat2_age_45to54.csv
 cp Formatter\ vmat2_female.csv data/vmat2_female.csv
 cp Formatter\ vmat2_male.csv data/vmat2_male.csv
-cp Formatter_VMAT2_dem.csv data/vmat2_dem.csv
+cp Formatter_VMAT2_dem.csv data/vmat2_breakdown_dem.csv
 
 sed -i 's/Site 10/UCSD/g' data/*.csv
 sed -i 's/Site 11/UPitt/g' data/*.csv
